68d5f1dd3dfa87153490de3f9d360378e7fadf47
SergeyRep <reprincevsergeyj@gmail.com>
2025-07-31 11:42:41 +0700

    q

--- BurgerApp/View/HomeScreen.swift
+++ BurgerApp/View/HomeScreen.swift
@@ -1,19 +1,14 @@
 import SwiftUI
 
 struct HomeScreen: View {
-  @StateObject private var filterBar = FilterViewModel()
   @StateObject private var mainViewModel = MainViewModel()
   
   var body: some View {
     VStack {
       LabelBar()
-      SearchBar()
-      FilterBar(vm: filterBar)
-        .onAppear {
-          filterBar.mainViewModel = mainViewModel
-        }
-      ProductGrid()
-        .environmentObject(mainViewModel)
+      SearchBar(vm: mainViewModel)
+      FilterBar(vm: mainViewModel)
+      ProductGrid(vm: mainViewModel)
     }
   }
 }
